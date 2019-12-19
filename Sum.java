292feb1cc39e5541885f6bb64f2fb6ef3aa525bc
mrseay24 <sidneyeseay@gmail.com>
2019-12-18 21:02:57 -0500

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sum;
+
+/**
+ *
+ * @author mrseay
+ */
+public class Sum {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    abstract class A{
+        abstract void sum();
+    }
+    class B extends A{
+        void sum(){
+            int
+                        
+        }
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        A ob = new A();
+        ob.sum();
+    }
+    
+}
